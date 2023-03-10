cf1b5d709f313a55a02126c98f2f44fb58b7548e
layred <layred.dota2@mail.ru>
2023-03-10 14:31:08 +0300

    change static settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -82,9 +82,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
